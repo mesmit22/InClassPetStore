32c0e9b605b10600050a61c39cb1e21431da73c9
Mark Smith <mesmit22@louisville.edu>
2016-03-23 18:27:45 -0400

    Added source code

---
+++ InClassPetStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InClassPetStore.Startup))]
+namespace InClassPetStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
